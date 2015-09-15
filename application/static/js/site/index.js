6c6665a26322c9508680d17dd48751b03c729ae5
hustlzp <hustlzp@qq.com>
2015-09-15 17:39:03 +0800

    Use loading button in index page.

--- application/static/js/site/index.js
+++ application/static/js/site/index.js
@@ -8,29 +8,30 @@ $.extend(g, {
     DAYS: 2
 });
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-        if (!g.loading) {
-            g.loading = true;
-            $('.loading-flag').fadeIn();
+$('.btn-load-more-pieces').click(function () {
+    var _this = $(this);
 
-            setTimeout(function () {
-                $.ajax({
-                    url: urlFor('piece.pieces_by_date'),
-                    method: 'post',
-                    dataType: 'json',
-                    data: {
-                        'start': g.startDate.format('YYYY-MM-DD'),
-                        'days': g.DAYS
-                    }
-                }).done(function (response) {
-                    $('.pieces-container').append(response.html);
-                    g.startDate = moment(response.start)
-                }).always(function () {
-                    g.loading = false;
-                    $('.loading-flag').fadeOut();
-                });
-            }, 800);
-        }
+    if (!g.loading) {
+        g.loading = true;
+
+        _this.addClass('loading');
+
+        setTimeout(function () {
+            $.ajax({
+                url: urlFor('piece.pieces_by_date'),
+                method: 'post',
+                dataType: 'json',
+                data: {
+                    'start': g.startDate.format('YYYY-MM-DD'),
+                    'days': g.DAYS
+                }
+            }).done(function (response) {
+                $('.pieces-container').append(response.html);
+                g.startDate = moment(response.start)
+            }).always(function () {
+                g.loading = false;
+                _this.removeClass('loading');
+            });
+        }, 800);
     }
 });
