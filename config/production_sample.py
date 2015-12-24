903139b85fb3b3889d8d42a2485a2f7828c3eff1
Zhipeng Liu <hustlzp@qq.com>
2015-12-24 17:12:10 +0800

    Update production_sample.py

--- config/production_sample.py
+++ config/production_sample.py
@@ -6,13 +6,13 @@ class ProductionConfig(Config):
     # App config
     SECRET_KEY = "\xb5\xb3}#\xb7A\xcac\x9d0\xb6\x0f\x80z\x97\x00\x1e\xc0\xb8+\xe9)\xf0}"
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 7
-    SESSION_COOKIE_NAME = '1jingdian_session'
+    SESSION_COOKIE_NAME = 'zidian_session'
 
     # Site domain
-    SITE_DOMAIN = "http://www.1jingdian.com"
+    SITE_DOMAIN = "http://zidian.tech"
 
     # Db config
-    SQLALCHEMY_DATABASE_URI = "mysql://root:password@host/1jingdian"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:password@localhost/zhongguan"
 
     # Upload set config
     UPLOADS_DEFAULT_URL = "%s/uploads/" % SITE_DOMAIN
@@ -21,7 +21,7 @@ class ProductionConfig(Config):
     SENTRY_DSN = ''
 
     # SendCloud config
-    SC_FROM = ''
+    SC_FROM = 'hi@zidian.com'
     SC_API_USER = ''
     SC_API_KEY = ''
 
